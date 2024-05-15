a8d4f75a8f79a577430171b5c5c51b005dcc7164
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-05-15 10:43:09 -0400

    api

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # hat-express-demo
+
+**Start server:**
+
+```sh
+npm start
+```
+
+App runs on http://localhost:3000.
+
+**Users:**
+
+http://localhost:3000/users
+
+**One user:**
+
+`http://localhost:3000/user/<id>`
--- server.js
+++ server.js
@@ -3,9 +3,32 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const users = [
+    {
+        id: 1,
+        name: "John",
+        department: "Engineering"
+    },
+    {
+        id: 2,
+        name: "Jane",
+        department: "Marketing"
+    }
+];
+
 // Get request
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get("/user/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    res.send(user);
+});
+
+app.get("/users", (req, res) => {
+    res.send(users);
 });
 
 app.listen(port, () => {

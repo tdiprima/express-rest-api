127aea73dbcde3d69c37928041339db5e320d616
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-05-15 12:07:04 -0400

    routes

--- README.md
+++ README.md
@@ -12,6 +12,46 @@ App runs on http://localhost:3000.
 
 http://localhost:3000/users
 
-**One user:**
+**Get user:**
 
 `http://localhost:3000/user/<id>`
+
+**Add user:**
+
+Use Thunder Client or Postman.
+
+POST: http://localhost:3000/user
+
+JSON Body:
+
+```json
+{
+  "id": 3,
+  "name": "Joe",
+  "department": "BMI"
+}
+```
+
+Then check it in http://localhost:3000/users
+
+**Update user:**
+
+PUT http://localhost:3000/user
+
+```json
+{
+  "id": 2,
+  "name": "Jane Doe-Smith",
+  "department": "Research"
+}
+```
+
+**Delete user:**
+
+DELETE http://localhost:3000/user/1
+
+Get departments:
+
+GET: http://localhost:3000/departments
+
+<br>
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json()); // transforms json input to object that we can work with
 
 const users = [
     {
@@ -16,7 +19,7 @@ const users = [
     }
 ];
 
-// Get request
+// GET requests
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
@@ -29,6 +32,60 @@ app.get("/user/:id", (req, res) => {
 
 app.get("/users", (req, res) => {
     res.send(users);
+});
+
+app.get("/departments", (req, res) => {
+    const departments = users.map(user => user.department);
+    const uniqueDepartments = [...new Set(departments)];
+
+    if (uniqueDepartments.length === 0) {
+        res.status(404).send({
+            data: { message: "No departments found" }
+        });
+    } else {
+        res.status(200).send({
+            data: { departments: uniqueDepartments, length: uniqueDepartments.length }
+        });
+    }
+});
+
+// POST request
+app.post("/user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    users.push(user);
+    res.send(user);
+});
+
+// PUT
+app.put("/user", (req, res) => {
+    const user = req.body;
+    const id = user.id;
+    const index = users.findIndex(u => u.id === id);
+
+    if (index !== -1) {
+        users[index] = user; // Update the user
+        res.send(user);
+    } else {
+        res.status(404).send({ message: "User not found" });
+    }
+});
+
+// DELETE
+app.delete("/user/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const index = users.findIndex(user => user.id === id);
+
+    if (index !== -1) {
+        users.splice(index, 1); // Remove the user from the array
+        res.status(200).send({
+            message: "User deleted successfully"
+        });
+    } else {
+        res.status(404).send({
+            message: "User not found"
+        });
+    }
 });
 
 app.listen(port, () => {
